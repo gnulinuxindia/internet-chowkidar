a0a609c9021e98d2d01ef83ba761ab2bd2e006e7
SphericalKat <me@kat.bio>
2025-12-19 13:05:40 +0530

    chore: add nix flake
    Signed-off-by: SphericalKat <me@kat.bio>

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "Flake for internet-chowkidar";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  inputs.systems.url = "sourcehut:~sphericalkat/flake-systems";
+
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.flake-compat.url = "git+https://git.lix.systems/lix-project/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs =
+    {
+      systems,
+      nixpkgs,
+      devshell,
+      ...
+    }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.devshell.mkShell {
+            bash = {
+              interactive = "";
+            };
+
+            env = [
+              {
+                name = "DEVSHELL_NO_MOTD";
+                value = 1;
+              }
+            ];
+
+            packages = with pkgs; [
+              git
+              go-outline
+              go
+              gopls
+              gotools
+            ];
+          };
+        }
+      );
+    };
+}
